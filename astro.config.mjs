a3d74911fae29ba7b34f99087cd08e96fb918aae
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-07 16:37:03 -0400

    2er Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-//import 'boxicons';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://profWilliamArte.github.io',
-	base: '/astro01',
+	base: '/astroportafolio',
 });
 
